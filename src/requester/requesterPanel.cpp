ff25423f681058e232785865a9a65b66b8d761bb
rendayigit <renda.yigit@gmail.com>
2025-08-10 18:14:43 +0300

    implementing requester

--- src/requester/requesterPanel.cpp
+++ src/requester/requesterPanel.cpp
@@ -1,8 +1,9 @@
 #include "requesterPanel.hpp"
 
 #include "requester.hpp"
-#include "wx/gdicmn.h"
 #include "wxConstants.hpp"
+
+#include <nlohmann/json.hpp>
 
 constexpr int ADDRESS_WIDTH = 200;
 constexpr int REQUEST_TEXT_AREA_WIDTH = 400;
@@ -21,8 +22,8 @@ RequesterPanel::RequesterPanel(wxWindow *parent)
   topSzr->Add(addressLbl, 0, WX_CENTER, wxSizerFlags::GetDefaultBorder());
   topSzr->Add(addressTxtCtrl, 0, WX_EXPAND, wxSizerFlags::GetDefaultBorder());
 
-  requestTxtCtrl
-      = new wxTextCtrl(this, wxID_ANY, "Enter your message here", wxDefaultPosition, wxSize(REQUEST_TEXT_AREA_WIDTH, -1), wxTE_MULTILINE);
+  requestTxtCtrl = new wxTextCtrl(this, wxID_ANY, "Enter your message here", wxDefaultPosition,
+                                  wxSize(REQUEST_TEXT_AREA_WIDTH, -1), wxTE_MULTILINE);
   messageSzr->Add(requestTxtCtrl, 0, WX_EXPAND, wxSizerFlags::GetDefaultBorder());
 
   responseTxtCtrl = new wxTextCtrl(this, wxID_ANY, "Response will be displayed here", wxDefaultPosition, wxDefaultSize,
@@ -48,7 +49,12 @@ void RequesterPanel::onSendRequest(wxCommandEvent &event) { // NOLINT(readabilit
 
   std::string response = Requester::getInstance().request(requestTxtCtrl->GetValue().ToStdString());
 
-  responseTxtCtrl->SetValue(response);
+  try {
+    nlohmann::json responseJson = nlohmann::json::parse(response);
+    responseTxtCtrl->SetValue(responseJson.dump(2));
+  } catch (const nlohmann::json::parse_error &) {
+    responseTxtCtrl->SetValue(response);
+  }
 
   event.Skip();
 }
